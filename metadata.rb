8a420c810bb51c86db6680a970b654b30c294aad
Mircea Preotu <mircea.preotu@gmail.com>
2014-09-17 16:24:56 +0200

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
-name 'wordpress'
-maintainer 'Mircea Preotu'
-maintainer_email 'Mircea Preotu <mircea.preotu@gmail.com>'
-license 'Apache 2.0'
-description 'Installs/Configures Wordpress'
-version '0.0.1'
+name             "wordpress"
+maintainer       "Mircea Preotu"
+maintainer_email "Mircea Preotu <mircea.preotu@gmail.com>"
+license          "Apache 2.0"
+description      "Installs/Configures WordPress"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0.0"
+
+recipe "wordpress::default", "Installs and configures WordPress with php-fpm + nginx + mysql stack"
 
 depends 'mysql'
